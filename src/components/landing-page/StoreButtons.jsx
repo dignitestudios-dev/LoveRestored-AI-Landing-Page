ad70464c5e119dba3c3b6b946068e63772c77652
kevinparker-dev <muhammadkamil.raza@launchbox.pk>
2026-01-08 13:07:43 +0500

    added links to store buttons

--- src/components/landing-page/StoreButtons.jsx
+++ src/components/landing-page/StoreButtons.jsx
@@ -2,35 +2,50 @@ import React from "react";
 import Button from "../Button";
 import GooglePlay from "../icons/GooglePlay";
 import Apple from "../icons/Apple";
+import Link from "next/link";
 
 const StoreButtons = () => {
   return (
     <>
-      <Button varient={"transparent"}>
-        <div className="flex sm:gap-3 gap-2 h-full items-center">
-          <div>
-            <GooglePlay />
+      <Link
+        target="_blank"
+        href={
+          "http://play.google.com/store/apps/details?id=com.dignitestudios.loverestoredai"
+        }
+      >
+        <Button varient={"transparent"}>
+          <div className="flex sm:gap-3 gap-2 h-full items-center">
+            <div>
+              <GooglePlay />
+            </div>
+            <div className="w-full flex flex-col items-start justify-between">
+              <p className="text-[6px] text-white/75 tracking-wider">
+                GET IT ON
+              </p>
+              <p>Google Play</p>
+            </div>
           </div>
-          <div className="w-full flex flex-col items-start justify-between">
-            <p className="text-[6px] text-white/75 tracking-wider">GET IT ON</p>
-            <p>Google Play</p>
-          </div>
-        </div>
-      </Button>
+        </Button>
+      </Link>
 
-      <Button varient={"transparent"}>
-        <div className="flex sm:gap-3 gap-2 h-full items-center">
-          <div>
-            <Apple />
+      <Link
+        target="_blank"
+        href={"https://apps.apple.com/us/app/loverestored-ai/id6751412290"}
+      >
+        <Button varient={"transparent"}>
+          <div className="flex sm:gap-3 gap-2 h-full items-center">
+            <div>
+              <Apple />
+            </div>
+            <div className="w-full flex flex-col items-start justify-between">
+              <p className="text-[6px] text-white/75 tracking-wider">
+                DOWNLOAD ON THE
+              </p>
+              <p>App Store</p>
+            </div>
           </div>
-          <div className="w-full flex flex-col items-start justify-between">
-            <p className="text-[6px] text-white/75 tracking-wider">
-              DOWNLOAD ON THE
-            </p>
-            <p>App Store</p>
-          </div>
-        </div>
-      </Button>
+        </Button>
+      </Link>
     </>
   );
 };
